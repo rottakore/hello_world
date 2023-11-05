a413a7736650354fe815ea494d9ad59e6c54b066
rottakore <88859658+rottakore@users.noreply.github.com>
2023-11-05 16:51:49 -0300

    Add files via upload
    All files

---
+++ target/debug/HelloWorld.d
@@ -0,0 +1,1 @@
+C:\Users\lu\Documents\Projetos\ e\ Anotações\Rust\HelloWorld\target\debug\HelloWorld.exe: C:\Users\lu\Documents\Projetos\ e\ Anotações\Rust\HelloWorld\src\main.rs
---
+++ target/debug/deps/HelloWorld.d
@@ -0,0 +1,5 @@
+C:\Users\lu\Documents\Projetos e Anotações\Rust\HelloWorld\target\debug\deps\HelloWorld.exe: src\main.rs
+
+C:\Users\lu\Documents\Projetos e Anotações\Rust\HelloWorld\target\debug\deps\HelloWorld.d: src\main.rs
+
+src\main.rs:
