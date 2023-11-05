a327d8bed43a269bc2800ce218295ac3d9a10b57
rottakore <88859658+rottakore@users.noreply.github.com>
2023-11-05 20:19:26 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,29 @@
+// Dependencies >>
 use std::io::{stdin, stdout, Read, Write};
+// << Dependencies
 
+// Pause function >>
 fn pause() {
 	let mut stdout = stdout();
-	stdout.write(b"").unwrap();
-	stdout.flush().unwrap();
-	stdin().read(&mut [0]).unwrap();
+	stdout
+		.write(b"")
+		.unwrap();
+	stdout
+		.flush()
+		.unwrap();
+	stdin()
+		.read(&mut [0])
+		.unwrap();
 }
+// << Pause function
 
+// HelloWorld function >>
 fn main() {
-    println!("Hello, world!");
+
+	// Message >>
+	println!("Hello, world!");
+	// << Message
+
 	pause();
 }
+// << HelloWorld function
