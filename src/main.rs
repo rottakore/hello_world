5e34d688e2116eed51760f30b7c2deb5588d499b
rottakore <88859658+rottakore@users.noreply.github.com>
2023-11-05 18:00:57 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::io::{stdin, stdout, Read, Write};
 
 fn pause() {
 	let mut stdout = stdout();
-	stdout.write("").unwrap();
+	stdout.write(b"").unwrap();
 	stdout.flush().unwrap();
 	stdin().read(&mut [0]).unwrap();
 }
