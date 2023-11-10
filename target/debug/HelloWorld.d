e06f5822aa07901a6610bcd56b5cc5db69e11fb8
rottakore <88859658+rottakore@users.noreply.github.com>
2023-11-10 11:49:36 -0300

    Delete target directory

--- target/debug/HelloWorld.d
+++
@@ -1,1 +0,0 @@
-C:\Users\lu\Documents\Projetos\ e\ Anotações\Rust\HelloWorld\target\debug\HelloWorld.exe: C:\Users\lu\Documents\Projetos\ e\ Anotações\Rust\HelloWorld\src\main.rs
--- target/debug/deps/HelloWorld.d
+++
@@ -1,5 +0,0 @@
-C:\Users\lu\Documents\Projetos e Anotações\Rust\HelloWorld\target\debug\deps\HelloWorld.exe: src\main.rs
-
-C:\Users\lu\Documents\Projetos e Anotações\Rust\HelloWorld\target\debug\deps\HelloWorld.d: src\main.rs
-
-src\main.rs:
